f17348b943aaafc0efa439a8963c6a48bb324d88
ASHUTOSH <ashutoshkr2652@gmail.com>
2026-04-04 02:23:43 +0530

    changed the redis server to use redis cloud

--- server/src/config/redis.js
+++ server/src/config/redis.js
@@ -1,30 +1,38 @@
 import { createClient } from "redis";
-import IORedis from "ioredis";
+import Redis from "ioredis";
 import { env } from "./env.js";
 import logger from "../utils/logger.js";
 
 
 // Node-redis (sessions + caching)
 const redis = createClient({
-  url: env.REDIS_URL,
+    username: env.REDIS_USERNAME,
+    password: env.REDIS_PASSWORD,
+    socket: {
+        host: env.REDIS_URL,
+        port: env.REDIS_PORT,
+    }
 });
 
 redis.on("connect", () => logger.info("Redis (node-redis) connected"));
-redis.on("error", (err) => logger.error("Redis error", err.message));
+redis.on("error", (err) => logger.error("Redis error", err));
 redis.on("reconnecting", () => logger.warn("Redis reconnecting"));
 
 await redis.connect();
 
 // ioredis (BullMQ)
-export const bullConnection = new IORedis(env.REDIS_QUEUE_URL, {
-  maxRetriesPerRequest: null,
+export const bullConnection = new Redis({
+  host: env.REDIS_URL,
+  port: env.REDIS_PORT,
+  username: env.REDIS_USERNAME,
+  password: env.REDIS_PASSWORD,
 });
 
 bullConnection.on("connect", () =>
   logger.info("Redis (ioredis) connected for BullMQ")
 );
 bullConnection.on("error", (err) =>
-  logger.error("ioredis error", err.message)
+  logger.error("ioredis error", err)
 );
 
 // Keys

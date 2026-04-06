28730f5c44af06f6a7f521ef5502c911670d5ffd
ASHUTOSH <ashutoshkr2652@gmail.com>
2026-04-07 00:13:41 +0530

    removed client from worker

--- worker/client/src/api/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({
-  baseURL: 'http://localhost:5000',  // Express server port (see server/.env PORT=5000)
-  withCredentials: true,
-})
-
-
-export default api
